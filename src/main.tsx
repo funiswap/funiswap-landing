9b46b1d06a50689ecd2b5012b1fb5a53363ccf58
LW06 <qq@qq.com>
2023-10-21 02:47:08 +0800

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,16 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { injectGlobal, ThemeProvider, theme } from "./theme";
-
-import Section from "./components/section";
-import Quote from "./components/quote";
-import QuoteAuthor from "./components/quote-author";
+import { injectGlobal, ThemeProvider, theme } from './theme';
+import App from './App';
 
 injectGlobal`
     * { margin: 0; padding: 0; }
 `;
 
-interface HelloProps {
-  compiler: string;
-  framework: string;
-}
-
-class Hello extends React.Component<HelloProps, {}> {
-  render() {
-    return (
-      <Section>
-        <Quote>
-          “I personally would rather do the existentially essential things in
-          life on foot. If you live in England and your girlfriend is in Sicily,
-          and it is clear you want to marry her, then you should walk to Sicily
-          to propose. For these things travel by car or aeroplane is not the
-          right thing.”
-          <QuoteAuthor>
-            Werner Herzog, <em>Of Walking in Ice</em>
-          </QuoteAuthor>
-        </Quote>
-      </Section>
-    );
-  }
-}
-
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <Hello compiler="TypeScript" framework="React" />
+    <App />
   </ThemeProvider>,
-  document.getElementById("app")
+  document.getElementById('app'),
 );
